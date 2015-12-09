b78f4a86d28fe6583298228aac16452a5e416527
Metel Patel <metelpatel@gmail.com>
2015-12-08 22:38:41 -0600

    hw finished

--- w02/d02/starter/js/main.js
+++ w02/d02/starter/js/main.js
@@ -4,6 +4,7 @@ var wep;
 var option;
 var run = "You run like the coward you are, and Utopia is defeated...";
 var cry = "You cry like a baby and the ogre kills you, Utopia is defeated...";
+var tiny = "You just sit there, the ogre doesn't see you, walks over you and destroys Utopia...";
 
 if (race === "human"){
   wep = prompt("Ah! but of course! What type of two handed weapon shall thou muster? (sword, or axe)").toLowerCase();{
@@ -39,17 +40,85 @@ if (race === "elf"){
   if (wep === "bow"){
     option = prompt("a bit cliche don't you think? Forget it! Here comes an ogre! What will you do? (shoot, run, cry)").toLowerCase();
     if (option === "shoot"){
-      console.log("You single handedly killed all the ogres and save Utopia!");
+      num = prompt("How many arrows do you have?")
+      if (num > 1) {
+        console.log("You single handedly killed all the ogres with " + num + " arrows and save Utopia!");
+      }
     }
-    if (option === "run"){
+    else if (option === "run"){
       console.log(run);
     }
-    if (option === "cry"){
+    else if (option === "cry"){
       console.log(cry);
     }
   }
-
   if (wep === "dagger"){
     console.log("You suck with a dagger and die. Utopia is defeated...");
   }
 }
+
+if (race === "dwarf"){
+  wep = prompt("Ah yes, spectacularly obvious with that beard of yours! What weapon shall you use? (gun, hammer)").toLowerCase();
+  if (wep === "gun"){
+    option = prompt("An ogre approaches, will you shoot it? (shoot, run, be tiny)").toLowerCase();
+    if (option === "shoot"){
+      console.log("You shoot the ogre and kill it... what did you expect? guns are cheap... no honor... you couldn't use a more honorable weapon could you? you should really rethink your life. Like seriously. What are you doing playing this game if you want to be a gnome with a gun. Anyway you won... whatever...");
+    }
+    else if (option === "run"){
+      console.log(run);
+    }
+    else if (option === "be tiny"){
+      console.log(tiny);
+    }
+  }
+  if (wep === "hammer"){
+    option = prompt("Ah! an honorable weapon! Here comes an ogre! What shall you do? (smash, run, be tiny)").toLowerCase();
+    if (option === "smash"){
+      console.log("You smash the ogre with a killing blow! Utopia is saved!");
+    }
+    else if (option === "run"){
+      console.log(run);
+    }
+    else if (option === "be tiny"){
+      console.log(tiny);
+    }
+  }
+}
+if (race === "gnome"){
+  wep = prompt("Ah yes, I can see that by your siz... I mean by your wisdom.. yes wisdom. Which weapon shall you use? (crossbow, mace)").toLowerCase();
+  if (wep === "crossbow"){
+    option = prompt("Good choice! Here comes an ogre! Quick what shall you do? (shoot, run, be tiny)").toLowerCase();
+    if (option === "shoot"){
+      console.log("You shoot the ogre and it dies. Utopia is saved!");
+    }
+    else if (option === "run"){
+      console.log(run);
+    }
+    else if (option === "be tiny"){
+      console.log(tiny);
+    }
+  }
+  if (wep === "mace"){
+    console.log("hm... ok... you realize mace was not invented yet? you are defenseless, and you are defeated. Utopia is defeated...");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
