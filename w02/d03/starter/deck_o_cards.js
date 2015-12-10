61622461f66080b3a3c52cb85372870e0cee4781
Metel Patel <metelpatel@gmail.com>
2015-12-09 21:07:28 -0600

    w02d03 hw completed

--- w02/d03/starter/deck_o_cards.js
+++ w02/d03/starter/deck_o_cards.js
@@ -1,4 +1,4 @@
-// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays. 
+// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
 function deck_o_cards() {
 	var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
@@ -6,16 +6,26 @@ function deck_o_cards() {
 
 	// Create the arrays for the deck, and shuffled deck
 	var cards = [];
-	// Make 52 card objects and store them in the "cards" array
+  var object = {};
 
-	// Shuffle the Deck
 
-	// Pull the top card
-
-	// Console log the results
+  // Make 52 card objects and store them in the "cards" array
+  for (var i = 0; i < suits.length; i++){
+    for (var j = 0; j < values.length; j++){
+      var card = {'suit':suits[i], 'value':values[j]};
+      cards.push(card);
+  }
 }
 
-// Fisher-Yates Shuffle 
+	// Shuffle the Deck
+  shuffle(cards);
+	// Pull the top card
+  var topCard = cards.shift();
+	// Console log the results
+  console.log(topCard);
+}
+deck_o_cards();
+// Fisher-Yates Shuffle
 // http://stackoverflow.com/a/6274398
 function shuffle(array) {
     var counter = array.length, temp, index;
