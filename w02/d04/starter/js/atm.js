1f267c19044a97fef7388714f087195a5fcdf28d
Metel Patel <metelpatel@gmail.com>
2015-12-10 22:04:12 -0600

    completed ATM HW with bonus

--- w02/d04/starter/js/atm.js
+++ w02/d04/starter/js/atm.js
@@ -1,4 +1,65 @@
 //Begin with the document ready function
+$(function(){
+  var checkingBalance = 0;
+  var savingsBalance = 0;
+
+  $('#depositChecking').on('click', function(){
+    var inputAmount = parseFloat($('#amountChecking').val());
+    if (savingsBalance < 0){
+      savingsBalance += inputAmount;
+      $('#savingsBalance').text("$" + parseFloat(savingsBalance));
+      }
+    else {
+      checkingBalance += inputAmount;
+      }
+
+    $('#checkingBalance').text("$" + parseFloat(checkingBalance));
+      if (checkingBalance > 1){
+        $('#checkingBalance').css('background-color', '#E3E3E3');
+      }
+      //$('#amountChecking').val('');
+  });
+
+  $('#withdrawChecking').on('click', function() {
+    inputAmount = parseFloat($('#amountChecking').val());
+    if (inputAmount > checkingBalance){
+        $('#checkingBalance').css('background-color', 'red');
+        savingsBalance -= (40 + inputAmount);
+        if (savingsBalance < 0){
+          $('#savingsBalance').css('background-color', 'red');
+        }
+        $('#savingsBalance').text("$" + parseFloat(savingsBalance));
+        }
+    else {
+    checkingBalance -= inputAmount;
+    }
+    $('#checkingBalance').text("$" + parseFloat(checkingBalance));
+    //$('#amountChecking').val('');
+  });
+
+  $('#depositSavings').on('click', function(){
+    var inputAmount = parseFloat($('#amountSavings').val());
+    savingsBalance += inputAmount;
+    $('#savingsBalance').text("$" + parseFloat(savingsBalance));
+    if (savingsBalance > 1){
+      $('#savingsBalance').css('background-color', '#E3E3E3');
+    }
+    //$('#amountSavings').val('');
+  });
+
+  $('#withdrawSavings').on('click', function() {
+    inputAmount = parseFloat($('#amountSavings').val());
+    if (inputAmount > savingsBalance){
+        $('#savingsBalance').css('background-color', 'red');
+        return;
+        }
+    else {
+      savingsBalance -= inputAmount;
+    }
+    $('#savingsBalance').text("$" + parseFloat(savingsBalance));
+    //$('#amountSavings').val('');
+  });
+});
 
 	//Begin work on the Checking Account
 
@@ -10,10 +71,10 @@
 
 				//Take that value and add it to the existing value in the checkingBalance div
 
-		//Checking account withdrawl funtion 
+		//Checking account withdrawl funtion
 
 			//Get value from the amountChecking input field
-			
+
 			//On click of the withdrawChecking button
 
 				// If that value is greater than the value in the account ignore that action
@@ -31,10 +92,10 @@
 
 		 			//Take that value and add it to the existing value in the savingsBalance div
 
-		//Savings account withdraw funtion 
+		//Savings account withdraw funtion
 
 		 		//Get value from the amountSavings input field
-				
+
 		 		//On click of the withdrawl button
 
 		 			//If that value is greater than the value in the account ignore that action
