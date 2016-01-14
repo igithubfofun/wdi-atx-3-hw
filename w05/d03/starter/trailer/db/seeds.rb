451da64a79fe74bb10405cf5cdd52f1e3f9d723b
Metel Patel <metelpatel@gmail.com>
2016-01-14 09:08:17 -0600

    added embedded trailers

--- w05/d03/starter/trailer/db/seeds.rb
+++ w05/d03/starter/trailer/db/seeds.rb
@@ -8,12 +8,15 @@
 Movie.destroy_all
 Character.destroy_all
 Movie.create([
-  {title: 'Spiderman', year: '2010', poster_url: 'http://www.theamazingspidermangame.com/etc/designs/atvi/amazing-spiderman-game/amazing-spiderman-game-2/images/features/img5.jpg'},
-  {title: 'Batman', year: '2012', poster_url: 'http://static2.gamespot.com/uploads/scale_large/104/1049837/2891179-batman-arkham_knight-review_nologo_20150618.jpg'},
-  {title: 'Spiderman vs. Batman', year: '2016', poster_url: 'https://i.ytimg.com/vi/e8K1m6SCRz4/hqdefault.jpg'}
+  {title: 'Spiderman', year: '2010', poster_url: 'http://www.youtube.com/embed/FN3YaybNJ2s?autoplay=1&origin=http://example.com'},
+  {title: 'Batman', year: '2012', poster_url: 'https://www.youtube.com/embed/gn-twZm_wEY?autoplay=1&origin=http://example.com'},
+  {title: 'Spiderman vs. Batman', year: '2016', poster_url: 'http://www.youtube.com/embed/yViIi3gie2c?autoplay=1&origin=http://example.com'}
 ])
 
 Character.create([
   {name: 'Tobey Maguire', photo_url: 'https://upload.wikimedia.org/wikipedia/commons/c/c2/Tobey_Maguire_2014.jpg'},
   {name: 'Michael Keaton', photo_url: 'http://ia.media-imdb.com/images/M/MV5BMTk4NTE2MzczOF5BMl5BanBnXkFtZTYwNTM4MjYz._V1_UY317_CR21,0,214,317_AL_.jpg'}
 ])
+
+
+# http://www.theamazingspidermangame.com/etc/designs/atvi/amazing-spiderman-game/amazing-spiderman-game-2/images/features/img5.jpg
