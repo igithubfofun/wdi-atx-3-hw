d5db997a284e2d7aef92ff4d19bbe7084989e3eb
Matt Duncan <busman84@gmail.com>
2016-02-03 10:20:03 -0600

    adding solution to twilio

--- w08/d02/starter/API_twilio/routes/index.js
+++ w08/d02/starter/API_twilio/routes/index.js
@@ -3,11 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
-router.post('/'), function(req, res, next) {
-	// Your code goes here
-}
+router.post('/', function(req, res, next) {
+    // Twilio Credentials 
+    var accountSid = 'Your accountSid Here';
+    var authToken = 'Your authToken here';
+    var recipient = req.body.to;
+    var textMessage = req.body.message;
 
+
+
+    //require the Twilio module and create a REST client 
+    // var client = require('twilio')(accountSid, authToken); 
+    var client = require('twilio')(accountSid, authToken);
+    client.messages.create({
+        to: recipient,
+        from: "+14697891441",
+        body: textMessage,
+    }, function(err, message) {
+        console.log(message.sid);
+    });
+
+    res.render('index', {
+        title: 'Express'
+    });
+});
 module.exports = router;
