ee5c6790073cfb227f32a149902129312a3e648f
Matthew Duncan <busman84@gmail.com>
2016-02-03 10:21:53 -0600

    Update index.js

--- w08/d02/solution/API-twilio/routes/index.js
+++ w08/d02/solution/API-twilio/routes/index.js
@@ -9,7 +9,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    
+    // Twilio Credentials 
+    var accountSid = 'Your accountSid Here';
+    var authToken = 'Your authToken here';
+    var recipient = req.body.to;
+    var textMessage = req.body.message;
+
+
+
+    //require the Twilio module and create a REST client 
+    // var client = require('twilio')(accountSid, authToken); 
+    var client = require('twilio')(accountSid, authToken);
+    client.messages.create({
+        to: recipient,
+        from: "+14697891441",
+        body: textMessage,
+    }, function(err, message) {
+        console.log(message.sid);
+    });
+
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
 
