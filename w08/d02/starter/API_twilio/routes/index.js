e9688bac66ceb293dae3ec8d8d41d55ff9ea1be2
Metel Patel <metelpatel@gmail.com>
2016-02-03 10:03:18 -0600

    testing

--- w08/d02/starter/API_twilio/routes/index.js
+++ w08/d02/starter/API_twilio/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var receipient = req.body.to;
+var textMessage = req.body.message;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +9,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/'), function(req, res, next) {
-	// Your code goes here
+	// Twilio Credentials
+  var accountSid = 'AC59037293157be6ad8c57825913ebddef';
+  var authToken = '1ffd40d287408c46ac2c69c82fdac4f1';
+
+  //require the Twilio module and create a REST client
+  var client = require('twilio')(accountSid, authToken);
+
+  client.messages.create({
+    to: receipient,
+    from: "+15126498066",
+    body: textMessage,
+  }, function(err, message) {
+    console.log(message.sid);
+  });
+  res.render('index', { title: 'Express' });
 }
 
 module.exports = router;
