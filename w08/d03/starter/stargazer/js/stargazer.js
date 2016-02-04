c72c2a773588f92d084167fd0b0fa35dd128fbe2
Metel Patel <metelpatel@gmail.com>
2016-02-03 20:09:38 -0600

    stargazer complete

--- w08/d03/starter/stargazer/js/stargazer.js
+++ w08/d03/starter/stargazer/js/stargazer.js
@@ -1,5 +1,34 @@
-function search(name) {
+$(function(){
   var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
-}
+  function search(name) {
+    var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
+    // requires "jsonp" datatype.
+  }
+
+    $('#search').on('submit', function(){
+      var planetSearch = $('#search-name').val();
+
+      console.log(api + planetSearch);
+      $.ajax({
+        method: "GET",
+        url: api,
+        dataType: 'jsonp',
+        data: {
+          name: planetSearch
+        }
+      })
+
+      .done(function(data){
+        console.log(data);
+
+        $('#preview').html("<img class='result' src='" + data["image"]["src"] + "'/>");
+
+      });
+      return false;
+    })
+
+
+
+
+
+});
